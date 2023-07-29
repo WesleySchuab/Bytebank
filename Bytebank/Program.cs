be13ddf03e67502ab674d00277ad9f2792e2d2db
Wesley Schuab <wesley.schuab@p42.com.br>
2023-07-29 08:44:20 -0300

    Criando um objeto

--- Bytebank/Program.cs
+++ Bytebank/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bytebank.bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-X";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
