bdaebd30a0bde48d85b37f8e220ec1729d154c71
Wesley Schuab <wesley.schuab@p42.com.br>
2023-07-29 15:56:58 -0300

    usando medoto sacar

--- Bytebank/Program.cs
+++ Bytebank/Program.cs
@@ -7,5 +7,9 @@ contaDoAndre.conta = "1010-X";
 contaDoAndre.saldo = 100;
 
 Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
+
 contaDoAndre.Depositar(100);
 Console.WriteLine( "Saldo pos deposito"+contaDoAndre.saldo);
+
+contaDoAndre.Sacar(50);
+Console.WriteLine("Saldo pos saque" + contaDoAndre.saldo);
