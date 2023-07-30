8feaf6e9a648b4b2624b8c72640c4a652700598d
Wesley Schuab <wesley.schuab@p42.com.br>
2023-07-30 12:20:07 -0300

    Revert "Propiedade auto implementada"
    This reverts commit 4aa16efa1061bdb4cb8167150c3c9c51fc07a1fa.

--- Bytebank/Titular/Cliente.cs
+++ Bytebank/Titular/Cliente.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bytebank.Titular
 {
-    public Class Cliente
+    public class Cliente
     {
-    public string Nome { get; set; }
-
-    public string Cpf { get; set; }
-
-    public string Profissao { get; set; }
-}
+        public string nome;
+        public string cpf;
+        public string profissao;
+    }
 }
